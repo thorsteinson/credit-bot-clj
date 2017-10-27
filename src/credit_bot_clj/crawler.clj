16b555193c2f580e4339e7478b8c2705908e65e6
Caleb Thorsteinson <caleb@thorsteinson.io>
2017-10-27 09:29:43 -0700

    Fixes amount extraction bug
    The code was designed for the wrong page, and the indices were therefore
    incorrect. The led to some null pointer exceptions that were being
    thrown by the formatter.

--- src/credit_bot_clj/crawler.clj
+++ src/credit_bot_clj/crawler.clj
@@ -59,45 +59,48 @@
 
 (defn extract-amounts [driver]
   (let [rows (query-all driver {:tag "tr"})
-        credit-row (get-element-text-el driver (get rows 1))
-        checking-row (get-element-text-el driver (get rows 2))]
+        credit-row (get-element-text-el driver (get rows 2))
+        checking-row (get-element-text-el driver (get rows 3))]
     {:credit (parse-money-line credit-row)
      :checking (parse-money-line checking-row)}))
 
-(defn start-driver [& {:keys [debug?] :or {debug? false}}]
+(defn start-driver [debug?]
   (if debug?
     (boot-driver :chrome {:path "chromedriver.exe"})
     (headless)))
 
-(defn make-crawler [user password]
-  (let [start-req (chan 1)
-        finish-res (chan 1)
-        mfa-code-res (chan 1)
-        mfa-code-req (chan 1)
-        status-out (chan 1)
-        amount-out (chan 1)
-        transaction-req (chan 1)
-        transaction-res (chan 1)
-        init-state {:started false}
-        state (atom init-state) ]
-    (async/go-loop []
-      (<! start-req)
-      (log/info "STARTING CRAWLER")
-      (swap! state assoc :started true)
-      (swap! state assoc :driver (start-driver))
-      (login (:driver @state) user password)
-      (while (mfa-page? (:driver @state))
-        (do
-          (>! mfa-code-req :req)
-          (log/info "REQUESTED MFA")
-          (enter-mfa-code (:driver @state) (<! mfa-code-res))))
-      (<! finish-res)
-      (log/info "STOPPING CRAWLER")
-      (quit (:driver @state))
-      (reset! state init-state)
-      (recur))
-    {:start-req start-req
-     :finish-res finish-res
-     :mfa-code-res mfa-code-res
-     :mfa-code-req mfa-code-req
-     :state state}))
+(defn make-crawler [user password & opts]
+  ([user password]
+    (make-crawler user password {:debug? false}))
+  ([user password opts]
+    (let [start-req (chan 1)
+          finish-res (chan 1)
+          mfa-code-res (chan 1)
+          mfa-code-req (chan 1)
+          status-out (chan 1)
+          amount-out (chan 1)
+          transaction-req (chan 1)
+          transaction-res (chan 1)
+          init-state {:started false}
+          state (atom init-state) ]
+      (async/go-loop []
+        (<! start-req)
+        (log/info "STARTING CRAWLER")
+        (swap! state assoc :started true)
+        (swap! state assoc :driver (start-driver (:debug? opts)))
+        (login (:driver @state) user password)
+        (while (mfa-page? (:driver @state))
+          (do
+            (>! mfa-code-req :req)
+            (log/info "REQUESTED MFA")
+            (enter-mfa-code (:driver @state) (<! mfa-code-res))))
+        (<! finish-res)
+        (log/info "STOPPING CRAWLER")
+        (quit (:driver @state))
+        (reset! state init-state)
+        (recur))
+      {:start-req start-req
+       :finish-res finish-res
+       :mfa-code-res mfa-code-res
+       :mfa-code-req mfa-code-req
+       :state state})))
