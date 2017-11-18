d880e10ead3eda3e1cd16e21677c493cfc5efedf
Caleb Thorsteinson <caleb@thorsteinson.io>
2017-11-18 00:22:03 +0000

    Adds functions which simplify making requests Simplifies how requests are made and wraps everything so that all you have to do is enter the request channel and the response channel. Timeouts are included by default so if you don't get a response quickly enough it raises an exception and everything crashes down.

--- src/credit_bot_clj/utils.clj
+++ src/credit_bot_clj/utils.clj
@@ -1,4 +1,5 @@
-(ns credit-bot-clj.utils)
+(ns credit-bot-clj.utils
+  (:require [clojure.core.async :as async :refer [go >! <! alts! timeout]]))
 
 (defn parse-money-line [text]
   (let [formatter (java.text.NumberFormat/getInstance java.util.Locale/ENGLISH)
@@ -7,3 +8,31 @@
          (re-find money-pattern)
          (second)
          (.parse formatter))))
+
+(defn make-request 
+  "Make a request to the given channel, and throw an exception if we exceed
+  the specified timeout"
+  ([t req-chan]
+    (make-request t req-chan :request))
+  ([t req-chan req-code]
+    (go
+      (let [result (alts! [[req-chan x]
+                          (timeout t)])]
+        (if result
+          result
+          (throw Exception. (str "Request timed out after " t " seconds")))))))
+
+(defn make-response [t res-chan]
+  (go
+    (let [result (alts! [res-chan
+                         (timeout t)])]
+      (if result
+        result
+        (throw Exception. (str "Request timeout after " t " seconds"))))))
+
+(defn get-response [t req-chan res-chan]
+  (make-request t req-chan)
+  (make-response t res-chan))
+
+(def req (partial make-request 3000))
+(def req-res (partial get-response 3000))
