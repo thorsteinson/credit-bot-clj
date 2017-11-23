5257714edaca7a133a878011a9d0a23b94481530
Caleb Thorsteinson <caleb@thorsteinson.io>
2017-11-23 07:32:58 +0000

    Adds override param  for payment

--- src/credit_bot_clj/crawler/actions.clj
+++ src/credit_bot_clj/crawler/actions.clj
@@ -2,7 +2,7 @@
   (:require [etaoin.api :refer :all]
             [credit-bot-clj.utils :refer [parse-money-line]]))
 
-(defn- get-page [{:keys [driver]}]
+(defn- get-page [{:keys [driver]} state]
   (let [LOGIN-URL "https://onlinebanking.becu.org/BECUBankingWeb/Login.aspx"
         MFA-URL "https://onlinebanking.becu.org/BECUBankingWeb/mfa/challenge.aspx"
         ACCOUNT-URL "https://onlinebanking.becu.org/BECUBankingWeb/Accounts/Summary.aspx"]
@@ -13,7 +13,7 @@
       ACCOUNT-URL :account
       :unknown)))
 
-(defn- login [{:keys driver credentials}]
+(defn- login [{:keys [driver credentials]} state]
   (let [LOGIN_URL "https://onlinebanking.becu.org/BECUBankingWeb/login.aspx"]  
     (go driver LOGIN_URL))
   (let [USERNAME-INPUT {:id "ctlSignon_txtUserID"}
@@ -30,7 +30,7 @@
       :login :login
       (throw Exception. (str "Login error recieved unknown keyword: " (get-page driver))))))
 
-(defn- login-with-code [{:keys driver code}]
+(defn- login-with-code [{:keys [driver code]} state]
   (let [CODE-INPUT {:id "challengeAnswer"}
         CONTINUE-BTN {:id "mfa_btnAnswerChallenge"}]
     (doto driver
@@ -42,9 +42,12 @@
       (throw Exception. "Redirected to unknown page" ))))
 
 ; TODO: Add a check here, how did I miss this?
-(defn- nav-to-credit [{:keys driver}]
+(defn- nav-to-credit [{:keys [driver]} state]
   (let [VISA-TABLE {:id "visaTable"}
         PAY-BTN {:id "btnPayNow"}
+        OTHER-AMOUNT-TOGGLE {:id "ctlWorkflow_rdoPrincipalOnly1"}
+        OTHER-AMOUNT-INPUT {:id "ctlWorkflow_txtAddTransferAmountCredit"}
+        STATEMENT-BALANCE-TOGGLE {:id "ctlWorkflow_rdoPayoff1"}
         CONTINUE-BTN {:id "ctlWorkflow_btnAddNext"}
         ACCOUNT-SELECT {:id "ctlWorkflow_ddlAddFromAccount"}]
     (doto driver
@@ -53,12 +56,16 @@
           (click [ACCOUNT-SELECT {:tag "option" :index 2}])
           (click CONTINUE-BTN))))
 
-(defn- pay [state]
+; TODO: Check that the params are nested properly
+(defn- pay [{{:keys [credit] :balances} :keys [amount-override]} state]
   (let [OTHER-AMOUNT {:id "ctlWorkflow_rdoPrincipalOnly1"}
         OTHER-INPUT {:id "ctlWorkflow_txtAddTransferAmountCredit"}
         FREQ-SELECT {:id "ctlWorkflow_ddlAddFrequency1"}
         CONTINUE-BTN {:id "ctlWorkflow_btnAddVerifyAddTransfer"}
-        CONFIRM_BTN {:id "ctlWorkflow_btnAddConfirmAddPayment"}]
+        CONFIRM_BTN {:id "ctlWorkflow_btnAddConfirmAddPayment"}
+        amount (if amount-override
+                 amount-override
+                 credit)]
     (doto driver
           (click OTHER-AMOUNT)
           (fill OTHER-INPUT amount)
@@ -74,14 +81,14 @@
     (if-not (re-find pattern text)
       (throw Exception. "Didn't recieve validation message from BECU" ))))
 
-(defn- extract-amounts [{:keys driver}]
+(defn- extract-amounts [{:keys [driver]} state]
   (let [rows (query-all driver {:tag "tr"})
         credit-row (get-element-text-el driver (get rows 2))
         checking-row (get-element-text-el driver (get rows 3))]
     {:credit (parse-money-line credit-row)
      :checking (parse-money-line checking-row)}))
 
-(defn start-driver! [{:keys debug?}]
+(defn start-driver! [{:keys debug?} state]
   (if debug?
     (boot-driver :chrome {:path "chromedriver.exe"})
     (headless)))
